a31332385e1f882f1944d57cafe35370ed44ea78
viniciusks <vinicius.kremer@gmail.com>
2018-01-30 15:10:41 -0200

    Travis - inicio conf 2.0

--- preparing-www.sh
+++ preparing-www.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-echo "oi"
+
+# Construir a imagem docker
+cd docker/
+docker build -t my_app .
+docker login
+viniciusks13
+Vinicius@12
+docker tag my_app viniciusks13/my_app
+docker push viniciusks13/my_app
+
+# Pegando imagem docker
+eval $(minikube docker-env)
+docker pull viniciusks13/my_app
+kubectl set image deployments/my-app my-app=viniciusks13/my_app
